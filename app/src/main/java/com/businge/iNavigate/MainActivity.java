4c7b8f27665d3d9c348569060be6aea207f21f7e
bbusinge <donbrayo91@gmail.com>
2020-07-01 02:31:13 +0300

    set actions in buttons

--- app/src/main/java/com/businge/iNavigate/MainActivity.java
+++ app/src/main/java/com/businge/iNavigate/MainActivity.java
@@ -3,14 +3,16 @@ package com.businge.iNavigate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView textView;
     Button letsGetPro,openWebPage,toast,dialPhone,viewIntent,showLocation;
     String TAG = "--Main_Activity--";
@@ -28,7 +30,16 @@ public class MainActivity extends AppCompatActivity {
         showLocation = (Button)findViewById(R.id.showLocation);
         Log.i(TAG, "--onCreate--");
 
+        letsGetPro.setOnClickListener(this);
+        openWebPage.setOnClickListener(this);
+        toast.setOnClickListener(this);
+        dialPhone.setOnClickListener(this);
+        viewIntent.setOnClickListener(this);
+        showLocation.setOnClickListener(this);
+
+
     }
+
     @Override
     protected void onStart(){
         super.onStart();
@@ -65,39 +76,61 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG,"-- onDestroy --");
     }
 
-    button.setOnItemClickListener(new AdapterView.onItemClickListener(){
-      @Override
-      public void onClick(View view){
+    @Override
+    public void onClick(View view){
 
-          switch (view.getId()){
-              case R.id.showLocation:
-                  textView.setText("You clicked on Show Location");
-          //                intent = new Intent(getApplicationContext(),MainActivity1.class);
-                  Log.d("TAG","Yo clicked on Show Location");
-                  break;
-              case R.id.openWebPage:
-                  textView.setText("You have clicked on Open WebPage");
-                  Log.d("TAG","You clicked on Open WebPage");
-                  break;
-              case R.id.letsGetPro:
-                  textView.setText("You have clicked on let's getPro");
-                  Log.d("TAG","You clicked on let's getPro");
-                  break;
-              case R.id.viewIntent:
-                  textView.setText("You have clicked on View Intent");
-                  Log.d("TAG","You clicked on View Intent");
-                  break;
-              case R.id.dialPhone:
-                  textView.setText("You have clicked on Open Dial Phone");
-                  Log.d("TAG","You clicked on Open Dial Phone");
-                  break;
-              case R.id.toast:
-                  textView.setText("You have clicked on Toast");
-                  Log.d("TAG","You clicked on Toast");
-                  break;
-              }
-    });
+        switch (view.getId()){
+            case R.id.showLocation:
+                textView.setText("You clicked on Show Location");
+                //                intent = new Intent(getApplicationContext(),MainActivity1.class);
+                Log.d("TAG","Yo clicked on Show Location");
+                Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: 1.7647646, 31.7860329. 2"));
+                Toast.makeText(getApplicationContext(),"Mujooga", Toast.LENGTH_SHORT).show();
+                startActivity(intent1);
+                break;
+            case R.id.openWebPage:
+                textView.setText("You have clicked on Open WebPage");
+                Log.d("TAG","You clicked on Open WebPage");
+                Intent intent2 = new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.schoolnetuganda.com/highsecondary/kabalega-secondary-school"));
+                Toast.makeText(getApplicationContext(),"Mujooga", Toast.LENGTH_SHORT).show();
+                startActivity(intent2);
+                break;
+            case R.id.letsGetPro:
+                textView.setText("You have clicked on let's getPro");
+                Log.d("TAG","You clicked on let's getPro");
+                Intent intent3 = new Intent(getApplicationContext(),MainActivity2.class);
+                Toast.makeText(getApplicationContext(),"Mujooga", Toast.LENGTH_SHORT).show();
+                startActivity(intent3);
+                break;
+            case R.id.viewIntent:
+                textView.setText("You have clicked on View Intent");
+                Log.d("TAG","You clicked on View Intent");
+                Intent intent4 = new Intent(MainActivity.this,MainActivity2.class);
+                intent4.putExtra("KEY", "Well done sir, data well received ");
+                Toast.makeText(getApplicationContext(),"Mujooga", Toast.LENGTH_SHORT).show();
+                startActivity(intent4);
+                break;
+            case R.id.dialPhone:
+                textView.setText("You have clicked on Open Dial Phone");
+                Log.d("TAG","You clicked on Open Dial Phone");
+                Intent intent5 = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:+256785459184"));
+                Toast.makeText(getApplicationContext(),"Mujooga", Toast.LENGTH_SHORT).show();
+                startActivity(intent5);
+                break;
+            case R.id.toast:
+                textView.setText("You have clicked on Toast");
+                Log.d("TAG","You clicked on Toast");
+                Intent intent6 = new Intent(Intent.ACTION_VIEW);
+                Toast.makeText(getApplicationContext(),"Mujooga", Toast.LENGTH_SHORT).show();
+                startActivity(intent6);
+
+                break;
+        }
+
+    }
+
+
 
 
             }
-}
+
--- app/src/main/java/com/businge/iNavigate/MainActivity2.java
+++ app/src/main/java/com/businge/iNavigate/MainActivity2.java
@@ -4,17 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
     String TAG = "--Main_Activity--";
+    //TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Log.i(TAG, "--Activity_Two--");
-//        Toast.makeText(this, " Activity 2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, " Activity 2", Toast.LENGTH_SHORT).show();
 
 
 //        receive data from mainActivity
